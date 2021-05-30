61efea7501ddd6a72d203bb7ccc2f080f0e1f8d0
Shivam Chahar <shivam.chahar@gmail.com>
2021-05-30 21:30:31 +0530

    created model with 3 layers

--- covital_ml_model.py
+++ covital_ml_model.py
@@ -120,3 +120,36 @@ print(X_train.shape)
 print(X_test.shape)
 print(y_train.shape)
 print(y_test.shape)
+
+# Model Creation
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense,Dropout,Activation,Flatten
+from tensorflow.keras.optimizers import Adam
+from sklearn import metrics
+
+# No of classes
+num_labels=y.shape[1]
+
+model=Sequential()
+
+###first layer
+model.add(Dense(100,input_shape=(40,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+###second layer
+model.add(Dense(200))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+###third layer
+
+model.add(Dense(100))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+###final layer
+model.add(Dense(num_labels))
+model.add(Activation('softmax'))
+
+print(model.summary())
+model.compile(loss='categorical_crossentropy',metrics=['accuracy'],optimizer='adam')
